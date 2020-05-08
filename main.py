b4e0cae5d9ec6b03e88d9016b00d511d1aa36d82
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-05-08 19:25:06 +0200

    Add jacobi on numpy

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
-def main():
-    print("python main function")
+#!/usr/bin/python
+
+import getopt
+import sys
+
+from numpy import zeros, diag, diagflat, dot
 
 
-if __name__ == '__main__':
-    main()
+def jacobi(A, b, N=25, x=None):
+    """Solves the equation Ax=b via the Jacobi iterative method."""
+    # Create an initial guess if needed
+    if x is None:
+        x = zeros(len(A[0]))
+
+    # Create a vector of the diagonal elements of A
+    # and subtract them from A
+    D = diag(A)
+    R = A - diagflat(D)
+
+    # Iterate for N times
+    for i in range(N):
+        x = (b - dot(R, x)) / D
+    return x
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print('Input file is "', inputfile)
+    print('Output file is "', outputfile)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
