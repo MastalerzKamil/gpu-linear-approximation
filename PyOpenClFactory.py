bad36ce3b6e25be1b09536b0eb43f4f7669afba9
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-05-13 09:25:59 +0200

    Add results into file

--- PyOpenClFactory.py
+++ PyOpenClFactory.py
@@ -8,13 +8,15 @@ import matplotlib.pyplot as plt
 
 
 class Lagrange:
-    def __init__(self, x_data, y_data, result_size):
+    def __init__(self, x_data, y_data):
         self.data_size = np.array([len(x_data)])
         self.x_vector = np.array(x_data).astype(np.double)
         self.y_vector = np.array(y_data).astype(np.double)
         self.delta_x = self.calculate_delta_x()
         self.start_value = self.get_first_x_vector_element()
         self.last_value = self.get_last_x_vector_element()
+        self.result_x_host_data = []
+        self.result_y_host_data = []
 
     def get_last_x_vector_element(self):
         return self.x_vector[len(self.x_vector) - 1]
@@ -26,11 +28,8 @@ class Lagrange:
         return self.x_vector[1] - self.x_vector[0]
 
     def execute(self, program_filename):
-        result_x_host_data = []
-        result_y_host_data = []
-
         for x in np.arange(self.start_value, self.last_value, self.delta_x):
-            result_x_host_data.append(x)
+            self.result_x_host_data.append(x)
             result_y_temp = np.array([1.0])
 
             cl_instance = CL()
@@ -41,13 +40,23 @@ class Lagrange:
                                             cl_instance.dest_buf)
             cl.enqueue_copy(cl_instance.queue, result_y_temp, cl_instance.dest_buf).wait()
             print(x, result_y_temp[0])
-            result_y_host_data.append(result_y_temp[0])
+            self.result_y_host_data.append(result_y_temp[0])
 
         print("\na", self.x_vector)
         print("b", self.y_vector)
-        print("result: ", result_y_host_data)
-        plt.plot(result_x_host_data, result_y_host_data, self.x_vector, self.y_vector)
+        print("result: ", self.result_y_host_data)
+        plt.plot(self.result_x_host_data, self.result_y_host_data, self.x_vector, self.y_vector)
         plt.show()
+
+    def write_to_file(self,filename):
+        n = len(self.result_x_host_data)
+        result_matrix = []
+
+        for i in range(n):
+            result_matrix.append([self.result_x_host_data[i], self.result_y_host_data[i]])
+        result_matrix = np.array(result_matrix)
+        print(result_matrix)
+        np.savetxt(filename, result_matrix)
 
 
 class CL:
@@ -73,5 +82,6 @@ class CL:
 if __name__ == "__main__":
     x_sample = [0, 1, 2, 3, 4, 5, 6]
     y_sample = [0, 1, 4, 9, 16, 25, 36]
-    example = Lagrange(x_sample, y_sample, 12)
+    example = Lagrange(x_sample, y_sample)
     example.execute("lagrangeInterpolate.cl")
+    example.write_to_file("result.txt")
